6c1b3450df83914fc9d0013bb7b7be7da965b792
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-20 19:54:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # Model building
 rf = RandomForestClassifier(max_depth=2, max_features=4, n_estimators=100, random_state=0)
+rf.fit(X_train, y_train)
 
 # Apply model to make predictions
 y_train_pred = rf.predict(X_train)
