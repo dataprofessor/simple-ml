f4da9ea9352f917099ab6cf1a28c29a013a46c5e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-20 19:36:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
+# Importing requisite libraries
 import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
-st.title('🎈 App Name')
+# Title of the app
+st.title('🎈 Simple ML App')
 
-st.write('Hello world!')
+# Load dataset
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/iris.csv')
+
+# Separate to X and y
+X = df.drop('Species', axis=1)
+y = df.Species
+
+# Data splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model building
+rf = RandomForestClassifier(max_depth=2, max_features=4, n_estimators=100, random_state=0)
+
+# Apply model to make predictions
