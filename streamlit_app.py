62e8928ccda194e9d586a66f68080649f373544b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-21 19:54:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,6 @@ st_test_size = st.sidebar.slider('Size of test set (test_size)', 0.1, 0.9, 0.2)
 st_max_features = st.sidebar.slider('Maximum number of features (max_features)', 1, 4, 4)
 st_n_estimators = st.sidebar.slider('Number of estimators (n_estimators)', 100, 1000, 200)
 
-
 # Separate to X and y
 X = df.drop('Species', axis=1)
 y = df.Species
@@ -42,16 +41,21 @@ y_test_pred = rf.predict(X_test)
 train_accuracy = accuracy_score(y_train, y_train_pred)
 test_accuracy = accuracy_score(y_test, y_test_pred)
 
-# Print data overview
+# Data overview
 st.subheader('Data overview')
 st.write(f'Number of samples (rows): `{df.shape[0]}`')
 st.write(f'Number of variables (columns): `{df.shape[1]}`')
 st.write(f'Variable names: `{list(df.columns)}`')
 
-df_species = df.Species.value_counts()
-st.bar_chart(df_species)
+# EDA
+data_sepal_length = qcut(df['Sepal.Length'], 4)
+data_species = df.Species.value_counts()
 
-# Print model performance
+st.write(data_sepal_length)
+
+st.bar_chart(data_species)
+
+# Model performance
 st.subheader('Model performance')
 st.write(f'Accuracy (Training set): `{train_accuracy}`')
 st.write(f'Accuracy (Test set): `{test_accuracy}`')
